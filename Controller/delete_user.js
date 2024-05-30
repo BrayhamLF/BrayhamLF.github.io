ad8d6d54b6ca09359ca6dc29b556f6bd2c063a05
Ibrahim <143204242+BrayhamLF@users.noreply.github.com>
2024-05-29 20:49:05 -0500

    Add files via upload

--- Controller/delete_user.js
+++ Controller/delete_user.js
@@ -1,4 +1,4 @@
-import { deleteuser, loginauth } from './firebase.js';
+import { deleteuser, loginauth, auth } from './firebase.js';
 
 const deleteUserForm = document.getElementById('DeleteUser-Form');
 
